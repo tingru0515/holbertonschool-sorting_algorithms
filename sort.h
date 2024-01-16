2c73a9d5fccb7d247cc75a4b1f8543b039389bdd
tingru0515 <7687@holbertonstudents.com>
2024-01-15 22:18:55 -0800

    add sort.h

---
+++ sort.h
@@ -0,0 +1,22 @@
+#ifndef SORT_H
+#define SORT_H
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* struct listint_s - doubly linked list node
+*
+* @n: integer stored in the node
+* @prev: pointer to the previous element of the list
+* @next: pointer to the next element of the list
+*/
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+void print_array(const int *array, size_t size);
+
+#endif /* SORT_H */
