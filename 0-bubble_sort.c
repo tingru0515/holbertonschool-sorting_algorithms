7e5dfa26fe5bed58e92d925ec7736a8a395928ab
tingru0515 <7687@holbertonstudents.com>
2024-01-16 01:01:43 -0800

    q0-q3

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,18 +7,21 @@
 */
 void bubble_sort(int *array, size_t size)
 {
-	unsigned int i, j;
+	int tmp;
+	size_t i, j;
 
-	if (!array)
+	if (array == NULL || size == 0)
 		return;
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size - i - 1; j++)
+		for (j = 0; j < size - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				swap(&array[j], &array[j + 1]);
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
 				print_array(array, size);
 			}
 		}
